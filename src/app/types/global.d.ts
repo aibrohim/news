726eaf7a4e745061531a7630c79cc95418a1ec87
Ibrohim <ibrohim7840@gmail.com>
2023-04-18 15:52:28 +0500

    added logo

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,6 +6,11 @@ declare module "*.scss" {
   export = classNames;
 }
 
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+
 declare module "*.svg" {
   const SVG: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default SVG;
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -4,19 +4,33 @@ import { ThemeToggler } from "features";
 
 import { classNames } from "shared/lib";
 import { AppLink, Container } from "shared/ui";
+import { Theme, useTheme } from "shared/config";
+
+import DarkLogo from "shared/assets/img/logo-dark.svg";
+import LightLogo from "shared/assets/img/logo-light.svg";
 
 import { navLinks } from "../constants";
 
 import classes from "./header.module.scss";
+
+const ThemeLogos = {
+  [Theme.LIGHT]: <DarkLogo />,
+  [Theme.DARK]: <LightLogo />,
+};
 
 interface HeaderProps extends HTMLAttributes<HTMLElement> {
   className?: string;
 }
 
 export const Header: FC = ({ className, ...props }: HeaderProps) => {
+  const { theme } = useTheme();
+
   return (
     <header className={classNames(classes.Header, {}, [className])} {...props}>
       <Container className={classes.Container}>
+        <AppLink to="/" className={classes.LogoLink}>
+          {ThemeLogos[theme]}
+        </AppLink>
         <nav className={classes.Nav}>
           <ul className={classes.NavList}>
             {navLinks.map((item) => (
