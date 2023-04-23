d4a1be340b0db3a7007d5aff4a7e6a9da644436e
Ibrohim <ibrohim7840@gmail.com>
2023-04-23 14:04:10 +0500

    first unit test

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,33 @@
+import { classNames } from "./classNames";
+
 describe("className", () => {
-  test("test", () => {
-    expect(true).toBe(true);
+  test("basic implementation", () => {
+    expect(classNames("class")).toBe("class");
+  });
+
+  test("with mods", () => {
+    expect(
+      classNames("class", {
+        hoverable: true,
+        mouseout: false,
+        clicked: undefined,
+      })
+    ).toBe("class hoverable");
+  });
+
+  test("with additional class", () => {
+    expect(classNames("class", {}, ["additionalClass"])).toBe(
+      "class additionalClass"
+    );
+  });
+
+  test("full implementation", () => {
+    expect(
+      classNames(
+        "class",
+        { hoverable: true, mouseout: false, clicked: undefined },
+        ["additionalClass"]
+      )
+    ).toBe("class additionalClass hoverable");
   });
 });
