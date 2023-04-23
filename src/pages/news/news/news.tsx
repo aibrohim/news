f8f07ae63e61b647419597659076728bb22aa0a4
Ibrohim <ibrohim7840@gmail.com>
2023-04-23 18:04:16 +0500

    added loader text feature

--- src/pages/news/news/news.tsx
+++ src/pages/news/news/news.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { Size } from "shared/constants/size";
 import { Container } from "shared/ui/container";
-import { Spinner } from "shared/ui/spinner";
+import { Direction, Spinner } from "shared/ui/spinner";
 
 const News: FC = () => {
   const { t } = useTranslation("news");
@@ -11,9 +11,11 @@ const News: FC = () => {
     <Container>
       <h1>{t("list.title")}</h1>
       <div style={{ display: "flex", alignItems: "center" }}>
-        <Spinner size={Size.LARGE} />
-        <Spinner size={Size.MEDIUM} />
-        <Spinner size={Size.SMALL} />
+        <Spinner size={Size.LARGE} direction={Direction.ROW}>
+          Hello
+        </Spinner>
+        <Spinner size={Size.MEDIUM}>Mello</Spinner>
+        <Spinner size={Size.SMALL}>Shmello</Spinner>
       </div>
     </Container>
   );
--- src/shared/ui/spinner/spinner.tsx
+++ src/shared/ui/spinner/spinner.tsx
@@ -1,29 +1,44 @@
-import { FC } from "react";
-import classes from "./spinner.module.scss";
+import { FC, ReactNode } from "react";
+
 import { classNames } from "shared/lib/classNames";
 import { Size } from "shared/constants/size";
+
+import classes from "./spinner.module.scss";
+
+export enum Direction {
+  COLUMN = "Column",
+  ROW = "Row",
+}
 
 interface SpinnerProps {
   size?: Size;
   className?: string;
+  direction?: Direction;
+  children?: ReactNode;
 }
 
 export const Spinner: FC<SpinnerProps> = ({
   size = Size.MEDIUM,
+  direction = Direction.COLUMN,
   className,
+  children,
 }) => {
   return (
-    <div
-      className={classNames(classes.Spinner, {}, [classes[size], className])}
-    >
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      {/* <div></div> */}
+    <div className={classNames(classes.Wrapper, {}, [classes[direction]])}>
+      <div
+        className={classNames(classes.Spinner, {}, [classes[size], className])}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <p className={classNames(classes.Text, {}, [classes[size]])}>
+        {children}
+      </p>
     </div>
   );
 };
